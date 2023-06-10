3e44d0a9e9838010de1ef708057f9a11d89b76db
Emil Huseynaliev <emilh1791@gmail.com>
2023-06-10 09:49:09 -0400

    Initial commit

---
+++ GroupmeClient.js
@@ -0,0 +1,61 @@
+const axios = require('axios');
+
+const DEFAULT_BOT_NAME = "New Bot";
+const DEFAULT_BOT_AVATAR_URL = "https://em-content.zobj.net/thumbs/160/apple/354/robot_1f916.png";
+
+module.exports = class GroupmeClient {
+    #apiToken;
+
+    constructor(apiToken) {
+        this.apiToken = apiToken;
+    }
+
+    async makeBot(groupId, name, avatarURL, callbackURL, dmNotification=true, active=true) {
+        const response = await axios({
+            method: 'post',
+            url: `https://api.groupme.com/v3/bots?token=${this.#apiToken}`,
+            data: {
+                bot: {
+                    group_id: groupId,
+                    name: name || DEFAULT_BOT_NAME,
+                    avatar_url: avatarURL || DEFAULT_BOT_AVATAR_URL,
+                    callback_url: callbackURL,
+                    dm_notification: dmNotification,
+                    active,
+                }
+            }
+        });
+        return response.data;
+    }
+
+    async downloadAndUploadImage(url) {
+        const downloadResponse = await axios({
+            method: 'get',
+            url,
+            responseType: 'arraybuffer',
+        });
+        const uploadResponse = await axios({
+            method: 'post',
+            url: 'https://image.groupme.com/pictures',
+            headers: {
+                'X-Access-Token': this.#apiToken,
+                'Content-Type': downloadResponse.headers.get('Content-Type'),
+            },
+            data: downloadResponse.data,
+        });
+        return uploadResponse.data.payload.url; // or .picture_url
+    }
+
+    async sendBotMessage(botId, text, attachments=[]) {
+        const response = await axios({
+            method: 'post',
+            url: `https://api.groupme.com/v3/bots/post?token=${this.#apiToken}`,
+            data: {
+                bot_id: botId,
+                text,
+                attachments,
+            },
+        });
+        return response.data;
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Discord <-> Groupme Sync (DGS)
+
+### Motivation
+
+TODO: add something about my stubborn friend here.
+
+### Pre-requisites
+
+- NodeJS (v16 or higher)
+
+### Getting Started
+
+```sh
+git clone git@github.com:emilh91/dgs.git # or via https
+cd dgs
+npm install
+cp .template.env .env
+# fill in .env (see next section)
+```
+
+### Environment Variables
+
+You can get Groupme-related tokens from https://dev.groupme.com/bots .  
+You can get Discord-related tokens from https://discord.com/developers/applications .  
+You will need to add your tokens and secrets to `.env`; this file is git-ignored.  
+Do not update `template.env` unless you know what you are doing.
